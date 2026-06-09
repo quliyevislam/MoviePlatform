2363c9c84065111753fb183077cd1c242a0002e6
quliyevislam <islam.quliyev.w@gmail.com>
2026-06-09 20:29:23 +0400

    feat(domain): add CreatedAt property to Review and Comment

--- src/MoviePlatform.Domain/Movies/Entities/Comment.cs
+++ src/MoviePlatform.Domain/Movies/Entities/Comment.cs
@@ -9,17 +9,19 @@ public sealed class Comment : BaseEntity<CommentId>
 	public UserId UserId { get; private set; }
 	public MovieId MovieId { get; private set; }
 	public CommentContent Content { get; private set; }
+	public DateTime CreatedAtUtc { get; private set; }
 
 	private Comment() { }
 
-	private Comment(UserId userId, CommentContent content)
+	private Comment(UserId userId, CommentContent content, DateTime createdAtUtc)
 	{
 		UserId = userId;
 		Content = content;
+		CreatedAtUtc = createdAtUtc;
 	}
 
 	public static Comment Create(UserId userId, CommentContent content)
 	{
-		return new(userId, content);
+		return new(userId, content, DateTime.UtcNow);
 	}
 }
--- src/MoviePlatform.Domain/Movies/Entities/Review.cs
+++ src/MoviePlatform.Domain/Movies/Entities/Review.cs
@@ -9,18 +9,20 @@ public sealed class Review : BaseEntity<ReviewId>
 	public UserId UserId { get; private set; }
 	public MovieId MovieId { get; private set; }
 	public ReviewScore Score { get; private set; }
+	public DateTime CreatedAtUtc { get; private set; }
 
 	private Review() { }
 
-	private Review(UserId userId, ReviewScore score)
+	private Review(UserId userId, ReviewScore score, DateTime createdAtUtc)
 	{
 		UserId = userId;
 		Score = score;
+		CreatedAtUtc = createdAtUtc;
 	}
 
 	public static Review Create(UserId userId, ReviewScore score)
 	{
-		return new(userId, score);
+		return new(userId, score, DateTime.UtcNow);
 	}
 
 	internal void UpdateScore(ReviewScore newScore)
