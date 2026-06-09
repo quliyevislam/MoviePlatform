9f28dfa1d0f8997bdd910be946f7adc0bfa2b062
quliyevislam <islam.quliyev.w@gmail.com>
2026-06-10 02:44:42 +0400

    refactor(domain): move value object creations to factory method

--- src/MoviePlatform.Domain/Users/User.cs
+++ src/MoviePlatform.Domain/Users/User.cs
@@ -18,8 +18,29 @@ public sealed class User : AggregateRoot<UserId>
 		PasswordHash = passwordHash;
 	}
 
-	public static User Create(Name name, Email email, PasswordHash passwordHash)
+	public static Result<User> Create(string name, string email, string passwordHash)
 	{
-		return new(name, email, passwordHash);
+		Result<Name> nameResult = Name.Create(name);
+
+		if (nameResult.IsFailure)
+		{
+			return Result.Failure<User>(nameResult.Error);
+		}
+
+		Result<Email> emailResult = Email.Create(email);
+
+		if (emailResult.IsFailure)
+		{
+			return Result.Failure<User>(emailResult.Error);
+		}
+
+		Result<PasswordHash> passwordHashResult = PasswordHash.Create(passwordHash);
+
+		if (passwordHashResult.IsFailure)
+		{
+			return Result.Failure<User>(passwordHashResult.Error);
+		}
+
+		return Result.Success<User>(new(nameResult.Value, emailResult.Value, passwordHashResult.Value));
 	}
 }
