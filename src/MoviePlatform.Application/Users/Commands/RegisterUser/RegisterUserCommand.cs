3e052d41a4c52f32ffdf6b2b72e8412660bff7cf
quliyevislam <islam.quliyev.w@gmail.com>
2026-06-15 19:22:49 +0400

    feat: add string trimming in the user queries and commands

--- src/MoviePlatform.Application/Users/Commands/RegisterUser/RegisterUserCommand.cs
+++ src/MoviePlatform.Application/Users/Commands/RegisterUser/RegisterUserCommand.cs
@@ -1,5 +1,19 @@
+using System.Text.Json.Serialization;
 using MoviePlatform.Application.Common.Messaging;
 
 namespace MoviePlatform.Application.Users.Commands.RegisterUser;
 
-public record RegisterUserCommand(string Name, string Email, string Password) : ICommand<int>;
+public record RegisterUserCommand : ICommand<int>
+{
+	public string Name { get; init; }
+    public string Email { get; init; }
+    public string Password { get; init; }
+
+    [JsonConstructor]
+    public RegisterUserCommand(string name, string email, string password)
+    {
+        Name = name.Trim();
+        Email = email.Trim();
+        Password = password.Trim();
+    }
+}
--- src/MoviePlatform.Application/Users/Queries/Login/LoginQuery.cs
+++ src/MoviePlatform.Application/Users/Queries/Login/LoginQuery.cs
@@ -1,5 +1,17 @@
+using System.Text.Json.Serialization;
 using MoviePlatform.Application.Common.Messaging;
 
 namespace MoviePlatform.Application.Users.Queries.Login;
 
-public record LoginQuery(string Email, string Password) : IQuery<LoginResponse>;
+public record LoginQuery : IQuery<LoginResponse>
+{
+	public string Email { get; init; }
+	public string Password { get; init; }
+
+    [JsonConstructor]
+	public LoginQuery(string email, string password)
+	{
+		Email = email.Trim();
+		Password = password.Trim();
+	}
+}
